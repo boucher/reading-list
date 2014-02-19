f43d1f056e1589fe8ae55e56b5bf987a01adadd7
boucher <rboucher@gmail.com>
2014-02-19 00:13:12 -0800

    another background test.

--- worker.py
+++ worker.py
@@ -4,108 +4,17 @@ import datetime
 import pymongo
 import requests
 import json
-import thread
 import time
-
-from flask import Flask
-from flask import abort, redirect, url_for, session, request, render_template
 
 from rauth.service import OAuth1Service, OAuth1Session
 from elementtree import ElementTree
 from bs4 import BeautifulSoup
 
-app = Flask(__name__)
-app.secret_key = os.environ['COOKIE_SECRET']
-
 mongo_client = pymongo.MongoClient(os.environ['MONGOHQ_URL'])
 mongo_db = mongo_client[os.environ['DB_NAME']]
 
-mongo_users = mongo_db['users']
-mongo_users.ensure_index([("user_id", pymongo.ASCENDING)], unique=True)
-
-mongo_pending_oauth = mongo_db['pending_oauth']
-mongo_pending_oauth.ensure_index([("created", pymongo.ASCENDING)], expireAfterSeconds=3600)
-
-mongo_book_details = mongo_db['books']
-mongo_book_details.ensure_index([("isbn13", pymongo.ASCENDING)], unique=True)
-
-mongo_sfpl_books = mongo_db['sfpl_books']
-mongo_sfpl_books.ensure_index([("isbn13", pymongo.ASCENDING)], unique=True)
-mongo_sfpl_books.ensure_index([("created", pymongo.ASCENDING)], expireAfterSeconds=30*24*60*60)
-
-mongo_availability = mongo_db['availability']
-mongo_availability.ensure_index([("isbn13", pymongo.ASCENDING)], unique=True)
-mongo_availability.ensure_index([("created", pymongo.ASCENDING)], expireAfterSeconds=3600)
-
 mongo_queue = mongo_db['availability_queue']
 mongo_queue.ensure_index([("user_id", pymongo.ASCENDING)], unique=True)
-
-@app.route('/')
-def index():
-    if session.get('user_id', None):
-        goodreads = goodreads_session(session['user_id'])
-        response = goodreads.get('https://www.goodreads.com/review/list', params={
-            'format': 'json',
-            'v': '2',
-            'user': session['user_id'],
-            'shelf': 'to-read'
-        })
-
-        isbns = [book['isbn13'] for book in response.json()]
-        count = mongo_availability.find({"isbn13":{"$in":isbns}})
-
-        if count.count() == len(isbns):
-            details = book_list_details(isbns, goodreads)
-            return render_template('index.html', user_id=session['user_id'], book_details=details)
-        else:
-            if not mongo_queue.find_one({"user_id":session['user_id']}):
-                mongo_queue.insert({"user_id":session['user_id']})
-            return render_template('loading.html')
-    else:
-        return redirect(url_for('login'))
-
-@app.route('/login')
-def login():
-    goodreads = goodreads_api()
-    request_token, request_token_secret = goodreads.get_request_token(header_auth=True)
-    authorize_url = goodreads.get_authorize_url(request_token, oauth_callback="http://"+os.environ['HOST']+"/callback")
-    mongo_pending_oauth.insert({"token": request_token, "secret":request_token_secret, "created":datetime.datetime.utcnow()})
-
-    return render_template('login.html', authorize_url=authorize_url)
-
-@app.route('/callback')
-def oauth_callback():
-    token = request.args.get('oauth_token', None)
-    authorized = request.args.get('authorize', None)
-
-    if authorized == '1':
-        details = mongo_pending_oauth.find_one({"token": token})
-        goodreads = goodreads_api()
-        goodreads_session = goodreads.get_auth_session(token, details['secret'])
-        access_token = goodreads_session.access_token
-        access_token_secret = goodreads_session.access_token_secret
-
-        response = goodreads_session.get('http://www.goodreads.com/api/auth_user', params={'format':'json'})
-        tree = ElementTree.fromstring(response.text.encode("utf-8"))
-        user_id = tree.find('user').attrib['id']
-
-        user = mongo_users.find_one({"user_id":user_id}) or {"user_id":user_id, "created":datetime.datetime.utcnow()}
-        user['access_token'] = access_token
-        user['access_token_secret'] = access_token_secret
-        mongo_users.save(user)
-
-        session['user_id'] = user_id
-
-        return redirect(url_for('index'))
-    else:
-        return redirect(url_for('login'))
-
-@app.route('/logout')
-def logout():
-    # remove the username from the session if it's there
-    session.pop('user_id', None)
-    return redirect(url_for('login'))
-
 
 def goodreads_session(user_id):
     user = mongo_users.find_one({"user_id":user_id})
@@ -280,7 +189,24 @@ def check_overdrive_availability(link):
     return result
 
 
-if __name__ == '__main__':
-    app.debug = os.environ['HOST'] == "127.0.0.1:5000"
-    app.run()
+def process_availability_queue():
+    while True:
+        print("Processing from the queue.")
+        for q in mongo_queue.find():
+            print(q)
+            goodreads = goodreads_session(q['user_id'])
+            response = goodreads.get('https://www.goodreads.com/review/list', params={
+                'format': 'json',
+                'v': '2',
+                'user': q['user_id'],
+                'shelf': 'to-read'
+            })
 
+            isbns = [book['isbn13'] for book in response.json()]
+            book_list_details(isbns, goodreads)
+            mongo_queue.remove({"user_id":q['user_id']})
+
+        time.sleep(5)
+
+if __name__ == '__main__':
+    process_availability_queue()
